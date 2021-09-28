d8f14b23bd7547749db42069cc6427eb3ace71f7
Wahyu Rudiyan Saputra <wahyu.rudiyan@ruangguru.com>
2021-09-28 23:59:18 +0700

    tested: able to consume message

---
+++ consumer-svc/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/wahyurudiyan/message-queue-example/consumer-svc/consumer"
+)
+
+func main() {
+	queueName := "myQueue"
+	consSvc := consumer.NewConsumer("guest", "guest", "localhost", "5672")
+
+	message, err := consSvc.ConsumeMessage(queueName)
+	if err != nil {
+		logrus.Errorf("[error] unable to consume: %s", err)
+	}
+
+	loop := make(chan bool)
+	go func() {
+		for m := range message {
+			data, _ := json.Marshal(m)
+			logrus.Printf("DATA : %s", string(data))
+		}
+	}()
+
+	fmt.Printf("[start] message started to listen, to exit pser CTRL+C\n\n")
+	<-loop
+}
